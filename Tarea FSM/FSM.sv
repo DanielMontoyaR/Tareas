2d8a267afba720fd8890138288b9b9effbb4657e
DanielMontoyaR <danimo.rivera@gmail.com>
2024-03-14 20:06:42 -0600

    FSM implementation

---
+++ Tarea FSM/FSM.sv
@@ -0,0 +1,68 @@
+module FSM(input clk, press, rst, 
+							 output reg [7:0] err, 
+							 output reg [7:0] count);
+
+logic [1:0] state, next_state;
+reg [7:0] count_reg;
+
+
+initial begin
+	state = 2'b00;
+	err = 8'h00;
+	count_reg = 8'h00;
+end
+
+
+//Actual state logic
+always@(posedge clk, posedge rst)
+begin
+	if(rst) begin
+		state = 2'b00;
+	end
+	else state=next_state;
+end	
+
+
+//Next state logic
+
+always@(posedge clk, posedge rst)
+begin
+	case(state)
+		2'b00:
+		begin
+			if(press) next_state = 2'b10;
+			else next_state = 2'b01;
+		end
+		2'b01:
+		begin
+			if(clk) next_state = 2'b11;
+			else next_state = 2'b00;
+		end
+		
+		2'b10:
+		begin
+			count_reg <= count_reg + 1;
+			next_state = 2'b00;
+		end
+		
+		2'b11:
+		begin
+			
+			if(rst) begin
+				err <= 8'h00;
+				next_state=2'b00;
+			end else begin
+				err <= 8'hFF;
+				next_state = 2'b11;
+			end 
+		end
+		default: next_state = 2'b00;
+	endcase
+end
+
+always @(*) begin
+	count = count_reg;
+end
+
+endmodule
+		
---
+++ Tarea FSM/FSM_tb.sv
@@ -0,0 +1,49 @@
+module FSM_tb;
+
+// Parameters
+parameter CLK_PERIOD = 10; // Clock period in time units
+
+// Signals
+reg clk = 0;        // Clock signal
+reg rst = 1;        // Reset signal (active high)
+reg press = 0;      // Input signal for button press
+wire [7:0] err;     // Output error signal
+wire [7:0] count;   // Output count signal
+
+// Instantiate the module under test
+FSM dut (
+    .clk(clk),
+    .rst(rst),
+    .press(press),
+    .err(err),
+    .count(count)
+);
+
+// Clock generation
+always #((CLK_PERIOD / 2)) clk = ~clk;
+
+// Stimulus
+initial begin
+	
+	rst=1;
+	press = 0;
+	#20
+	
+	rst = 0;
+	press = 0;
+	#20
+	
+	rst=0;
+	press=1;
+	#20
+	
+	rst=0;
+	press =0;
+	clk = 1;
+	#20
+	
+	rst=1;
+	
+end
+
+endmodule
